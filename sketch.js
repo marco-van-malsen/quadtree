2fe706784f5cba16361a2eb7fde2e8af30267b86
Marco van Malsen <37261543+marco-van-malsen@users.noreply.github.com>
2019-12-17 21:36:42 +0100

    add check for device roation

--- sketch.js
+++ sketch.js
@@ -8,23 +8,29 @@
 // For more:
 // https://github.com/CodingTrain/QuadTree
 
+var devOrient;
 var header = 15;
-var particles = [];
+var particles;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  devOrient = deviceOrientation;
+  particles = [];
   addParticles(50);
 }
 
 function draw() {
+  // read device orientation
+  if (devOrient && devOrient != deviceOrientation) setup();
+
   // background
   background(0);
 
   // get current framerate
-  var fpsNow = frameRate();
+  var fps = frameRate();
 
   // add more particles if framerate allows
-  if (fpsNow > 60) addParticles(50);
+  if (fps > 60) addParticles(50);
 
   // show stats in header
   fill(255);
@@ -34,7 +40,7 @@ function draw() {
   textAlign(CENTER, CENTER);
   text("QUAD-TREE", 0.5 * width, 0.5 * header);
   textAlign(RIGHT, CENTER);
-  text(int(fpsNow) + " FPS", width - 5, 0.5 * header);
+  text(int(fps) + " FPS", width - 5, 0.5 * header);
 
   // setup quad-tree
   var boundary = new Rectangle(width * 0.5, height * 0.5, width, height);
@@ -63,6 +69,5 @@ function draw() {
 
 // detect window resize
 function windowResized() {
-  particles = [];
   setup();
 }
